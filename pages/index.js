6ba57990a02d38c76907c3b2fa39b21a7e11c22c
marcelo <marcelomata91@gmail.com>
2021-07-05 17:09:31 -0500

    new homepage

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,3 @@
-import Container from '../components/container'
-import { getAllPostsForHome } from '../lib/api'
-import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
 import mstyles from '../styles.module.css'
 import Header from '../components/homeHeader'
 import HeroHome from '../components/hero-home'
@@ -11,9 +7,7 @@ import DesignSection from '../components/design-section'
 import DevelopmentSection from '../components/development-section'
 import EcomSection from '../components/ecom-section'
 
-export default function Index({ allPosts, preview }) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+export default function Index() {
   return (
     <div >
       
@@ -63,10 +57,4 @@ export default function Index({ allPosts, preview }) {
   )
 }
 
-export async function getStaticProps({ preview = false }) {
-  const allPosts = await getAllPostsForHome(preview)
-  return {
-    props: { allPosts, preview },
-    revalidate: 1
-  }
-}
+
